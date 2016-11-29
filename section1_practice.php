6a138fbb2885535f71a991ea390936a90d4b1437
Sheena Takkenberg <sheena.takkenberg@gmail.com>
2016-11-29 20:44:41 +1100

    section one practice exercise: printing hello to the screen using echo

---
+++ section1_practice.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Document</title>
+  </head>
+  <body>
+
+    <?php
+    // This is printing "Hello!" to the screen with h1 tags.
+    echo "<h1>Hello!</h1>"; ?>
+
+  </body>
+</html>
