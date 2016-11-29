c14b4414958844ff18bc9284b248a8e83365151a
Sheena Takkenberg <sheena.takkenberg@gmail.com>
2016-11-29 20:32:03 +1100

    Learnt how to do comments

---
+++ comments.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Document</title>
+  </head>
+  <body>
+
+    <?php
+
+    /*This function below displays text
+    This is the second line. */
+
+    //This is a single line comment
+    
+    echo "Hello, World";
+
+     ?>
+
+  </body>
+</html>
