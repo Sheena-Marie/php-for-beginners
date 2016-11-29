85cb13e3cbddd7c4c2cbec8218de5d93dc5c7e11
Sheena Takkenberg <sheena.takkenberg@gmail.com>
2016-11-29 20:23:39 +1100

    Learnt a little about Dynamic Text

---
+++ dynamic_text.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Document</title>
+  </head>
+  <body>
+
+    <?php
+
+    $title = "Sheena's Official Site"
+     ?>
+
+    <h1><?php echo $title ?></h1>
+
+  </body>
+</html>
