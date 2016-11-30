7349cec2654f60d3d14fe87576065283b1133442
Sheena Takkenberg <sheena.takkenberg@gmail.com>
2016-11-30 21:07:26 +1100

    variables

---
+++ variables.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Document</title>
+  </head>
+  <body>
+
+    <?php
+
+    $name = 'Sheena';
+    $number = 100;
+    $Number_List = 100;
+    $number_List = 300;
+    $NUMBER = 100;
+
+    // This is an example of concatenation. You use a . to join things together.
+    // DO NOT forget to add an empty string between, otherwise everything gets squooshed together.
+    echo $name . ' ' . $number;
+
+    $name = '<h1> Hello</h1>';
+
+    echo $name;
+
+
+     ?>
+
+  </body>
+</html>
