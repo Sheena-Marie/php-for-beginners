fe5a6ab06e5be289f3bdfb7f091294ad1b033428
Sheena Takkenberg <sheena.takkenberg@gmail.com>
2016-11-30 21:23:53 +1100

    math

---
+++ math.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Document</title>
+  </head>
+  <body>
+
+
+    <?php
+
+    echo 56 + 45;
+    echo "<br>";
+    echo 56 - 45;
+    echo "<br>";
+    echo 56 * 45;
+    echo "<br>";
+    echo 56 / 45;
+    echo "<br>";
+
+
+    echo 45 + 34 + 45 / 421 - 45;
+    echo "<br>";
+
+    echo 5 + 5 * 10;
+
+    echo (5 + 5) * 10;
+
+    $number = 12;
+    $number2 = 24;
+
+    echo $number + $number2;
+
+
+
+
+
+     ?>
+
+  </body>
+</html>
