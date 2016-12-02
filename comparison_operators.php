a13134fe5184397e03598569c3525c420aeebd63
Sheena Takkenberg <sheena.takkenberg@gmail.com>
2016-12-02 20:01:44 +1100

    comparison operators

---
+++ comparison_operators.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Document</title>
+  </head>
+  <body>
+
+    <h2>Comparison Operators</h2>
+
+    <pre>
+      equal ==
+      identical ===
+      compare > < >= <= <>
+      not equal !=
+      not identical !==
+
+    </pre>
+
+    <h2>Logical Operators</h2>
+    <pre>
+      And &&
+      Or ||
+      Not !
+    </pre>
+
+    <?php
+
+    if (4 === "4" || 5 < 10) {
+      echo "It is true";
+    }
+
+
+
+
+
+
+    ?>
+
+
+
+  </body>
+</html>
