5fca98f5b8ec9101a4771dbd3be68fd8422bd229
Sheena Takkenberg <sheena.takkenberg@gmail.com>
2016-12-02 19:46:16 +1100

    if statements

---
+++ if_statements.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Document</title>
+  </head>
+  <body>
+
+    <?php
+
+    //If statements
+    if(3 > 10){
+      echo "Three is less than ten.";
+    } elseif (4 > 5) {
+      echo "Of course four is less than 5";
+    } else {
+      echo "It is not";
+    }
+
+
+
+    ?>
+
+  </body>
+</html>
