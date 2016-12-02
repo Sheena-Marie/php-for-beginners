da926aac6ccd22a57662c36b8ce3da3139caa361
Sheena Takkenberg <sheena.takkenberg@gmail.com>
2016-12-02 18:48:59 +1100

    arrays

---
+++ arrays.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Document</title>
+  </head>
+  <body>
+
+    <?php
+
+    // This is an array
+    $numberList = array(23, 64, 267, 478, 267, 8765, 345, '5345', 345, '<h1>Hello</h1>');
+
+    // This prints an individual value from the array
+    echo $numberList[9];
+
+    // This prints the whole array to the screen.
+    print_r($numberList);
+     ?>
+
+  </body>
+</html>
