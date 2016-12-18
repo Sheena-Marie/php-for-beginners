75d1415d8e9003a22a0249e719071a1e360e4e37
Sheena Takkenberg <sheena.takkenberg@gmail.com>
2016-12-18 20:44:38 +1100

    For Loops

---
+++ forloop.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Document</title>
+  </head>
+  <body>
+
+    <?php
+
+    for ($counter = 0; $counter <=  10; $counter++) {
+      echo $counter . "<br>";
+    }
+
+    ?>
+
+  </body>
+</html>
