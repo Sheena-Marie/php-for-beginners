3f7c9fcae7880a9a8ad9ecee67b9b479ed2030fb
Sheena Takkenberg <sheena.takkenberg@gmail.com>
2016-12-18 20:34:59 +1100

    learnt While Loops

---
+++ whileloop.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>While Loops</title>
+  </head>
+  <body>
+
+    <?php
+
+    $counter = 0;
+    while ($counter <= 10) {
+      echo $counter;
+      // $counter = $counter + 1;
+      $counter++;
+      // above is the shorthand version
+    }
+
+
+    ?>
+
+  </body>
+</html>
