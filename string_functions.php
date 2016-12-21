091487cef0142414ba0ebbffdc19f023ae1caae1
Sheena Takkenberg <sheena.takkenberg@gmail.com>
2016-12-21 18:31:52 +1100

    string functions

---
+++ string_functions.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Document</title>
+  </head>
+  <body>
+
+    <?php
+
+    $string = "Hello, Sheena. How's things going?";
+
+    // Length of the string
+    echo strlen($string);
+
+    echo "<hr>";
+
+    // All to caps
+    echo strtoupper($string);
+
+    echo "<hr>";
+
+    // string to all lower case
+    echo strtolower($string);
+
+    echo "<hr>";
+
+
+    ?>
+
+  </body>
+</html>
