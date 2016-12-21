c3d3251f67044ee150abc008193b62db2b3fd87d
Sheena Takkenberg <sheena.takkenberg@gmail.com>
2016-12-21 20:13:22 +1100

    working some more on  and forms

--- form.php
+++ form.php
@@ -14,8 +14,37 @@ if(isset($_POST['submit'])) {
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-echo "Hello, " . $username . '. ';
-echo "Your password is: " . $password;
+$names = array('Sheena', 'Edwin', 'Peter', 'Samid', 'Mohad', 'Maria', 'Jane', 'Tom');
+
+$minimum = 5;
+$maximum = 10;
+
+// echo "Hello, " . $username . '. ';
+// echo "Your password is: " . $password;
+
+// if (strlen($username) < $minimum) {
+//   echo "Username has to be longer than 5 characters";
+// }
+//
+// if (strlen($username) > $maximum) {
+//   echo "Username must be less than 10 characters";
+// }
+//
+// if (!in_array($username, $names)) {
+//   echo "Sorry, you're not allowed to log in";
+// } else {
+//   echo "Welcome!" . $username;
+// }
+
+if(strlen($username) < $minimum) {
+  echo "Username has to be longer than 5 characters";
+} elseif (strlen($username) > $maximum) {
+  echo "Username has to be 10 characters or less";
+} elseif (!in_array($username, $names)) {
+  echo "Sorry, that Username was not found";
+} else {
+  echo "Welcome, " . $username;
+}
 
 }
 
