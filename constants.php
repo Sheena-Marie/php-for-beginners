7e1226f88ac5ec535824586c7c2c828a39171871
Sheena Takkenberg <sheena.takkenberg@gmail.com>
2016-12-21 16:14:39 +1100

    constants

---
+++ constants.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Document</title>
+  </head>
+  <body>
+
+    <?php
+
+    $number = 10;
+    
+    echo $number . "<hr>";
+
+    define("NAME", 1000);
+
+    echo NAME;
+
+    ?>
+
+  </body>
+</html>
