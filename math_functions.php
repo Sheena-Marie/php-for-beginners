42340d0fc32e3e0923ef43eb7d9ce6b9969af39b
Sheena Takkenberg <sheena.takkenberg@gmail.com>
2016-12-21 18:18:47 +1100

    math functions

---
+++ math_functions.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Document</title>
+  </head>
+  <body>
+
+    <?php
+
+    // power
+    echo pow(2, 7);
+
+    echo "<hr>";
+
+    // random number between 1 - 1000;
+    echo rand(1, 1000);
+
+    echo "<hr>";
+
+    // square root
+    echo sqrt(100);
+
+    echo "<hr>";
+
+    // rounds up to the nearest integer
+    echo ceil(4.1);
+
+    echo "<hr>";
+
+    // rounds down to the nearest integer
+    echo floor(4.6);
+
+    echo "<hr>";
+
+    // rounds a float up or down depending on the second number
+    echo round(4.6);
+    echo "<br>";
+    echo round(4.1);
+
+
+
+    ?>
+
+  </body>
+</html>
