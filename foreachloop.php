61b1a9fd970845d9a57d84c0040a21eeced248b9
Sheena Takkenberg <sheena.takkenberg@gmail.com>
2016-12-21 14:04:05 +1100

    foreach loop

---
+++ foreachloop.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Document</title>
+  </head>
+  <body>
+
+    <?php
+
+    $numbers = array(345, 397, 676, 385, 2657, 5784);
+
+    foreach ($numbers as $number) {
+      echo $number . "<br>";
+    }
+
+
+
+    ?>
+
+  </body>
+</html>
