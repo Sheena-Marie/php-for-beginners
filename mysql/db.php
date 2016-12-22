210daf4fda877d58429270f7ccaba394d8f31558
Sheena Takkenberg <sheena.takkenberg@gmail.com>
2016-12-22 19:43:25 +1100

    updating the database with PHP & mysql

---
+++ mysql/db.php
@@ -0,0 +1,9 @@
+<?php
+
+$connection = mysqli_connect('localhost', 'root', '', 'loginapp');
+
+if(!$connection) {
+  die('Database connection failed!');
+}
+
+?>
---
+++ mysql/login_update.php
@@ -0,0 +1,58 @@
+<?php include "db.php";
+
+    $query = "SELECT * FROM users";
+
+    $result = mysqli_query($connection, $query);
+
+    if (!$result) {
+      die('query FAILED' . mysqli_error($connection));
+    }
+
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Document</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha   384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+  </head>
+  <body>
+
+    <div class="container">
+      <div class="col-sm-6">
+
+          <form class="" action="login_update.php" method="post">
+            <div class="form-group">
+              <label for="username">Username</label>
+              <input type="text" name="username" class="form-control">
+            </div>
+
+            <div class="form-group">
+              <label for="password">Password</label>
+              <input type="password" name="password" class="form-control">
+            </div>
+
+            <div class="form-group">
+              <select class="" name="">
+                <option value="">1</option>
+              </select>
+            </div>
+
+            <input type="submit" name="submit" value="Update" class="btn btn-info">
+
+          </form>
+
+      </div>
+    </div>
+
+  </body>
+</html>
