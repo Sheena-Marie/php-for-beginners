2aeaaab2c2f5e1bb5e1286f75b32b54e834ce956
Sheena Takkenberg <sheena.takkenberg@gmail.com>
2016-12-23 18:07:01 +1000

    worked more with cookies

--- cookies.php
+++ cookies.php
@@ -12,9 +12,18 @@ setcookie($name, $value, $expiration);
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Document</title>
+    <title>Cookies</title>
   </head>
   <body>
+
+    <?php
+    if(isset($_COOKIE['MyFirstCookie'])){
+      $someOne = $_COOKIE['MyFirstCookie'];
+      echo $someOne;
+    } else {
+      $someOne = "Cookie not working";
+    }
+    ?>
 
   </body>
 </html>
