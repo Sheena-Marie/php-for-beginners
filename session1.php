d73597d434c6c7f001ae2873dc580ce88989301f
Sheena Takkenberg <sheena.takkenberg@gmail.com>
2016-12-23 18:21:23 +1000

    learnt about sessions

---
+++ session1.php
@@ -0,0 +1,16 @@
+<?php
+  session_start();
+
+  $_SESSION['greeting'] = "Hi, Sheena! I can't believe this is actually working!";
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Document</title>
+  </head>
+  <body>
+
+  </body>
+</html>
---
+++ session2.php
@@ -0,0 +1,16 @@
+<?php
+  session_start();
+  
+  echo $_SESSION['greeting'];
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Document</title>
+  </head>
+  <body>
+
+  </body>
+</html>
